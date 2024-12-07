2358e3788676656a43e75d454c6fb849b2a1c671
Shashank Singh <shashanksingh@Shashanks-MacBook-Air.local>
2024-12-07 19:17:06 +0530

    auto

--- src/main/java/com/example/hello_world/HelloWorldApplication.java
+++ src/main/java/com/example/hello_world/HelloWorldApplication.java
@@ -12,7 +12,7 @@
 
 // }
 ////--------
-package com.example.helloworld;
+package com.example.hello_world;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

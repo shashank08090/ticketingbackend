4f503225be84b02e8c6dddff2a251feaefa1d59f
Shashank Singh <shashanksingh@Shashanks-MacBook-Air.local>
2024-12-07 13:35:17 +0530

    crud operation

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -1,17 +1,4 @@
-// package com.example.hello_world;
 
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-// @SpringBootApplication
-// public class HelloWorldApplication {
-
-// 	public static void main(String[] args) {
-// 		SpringApplication.run(HelloWorldApplication.class, args);
-// 	}
-
-// }
-////--------
 package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
